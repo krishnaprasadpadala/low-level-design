f8460d4d419f95b34889d7c2b1e63938f039bbd8
krishnaprasadpadala <krishnaprasad.padala@outlook.com>
2022-10-27 22:35:57 +0530

    Added Getters and Settters

--- src/main/java/com/java/lld/solid/Bird.java
+++ src/main/java/com/java/lld/solid/Bird.java
@@ -32,4 +32,36 @@ public class Bird {
             System.out.println("Eagle is flying...");
         }
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
