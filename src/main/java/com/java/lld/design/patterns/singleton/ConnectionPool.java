e766e2df08a9735d5cdebabb83e7533eb69cc9a8
krishnaprasadpadala <krishnaprasad.padala@outlook.com>
2023-11-02 00:07:39 +0530

    initializing the instance on-demand

--- src/main/java/com/java/lld/design/patterns/singleton/ConnectionPool.java
+++ src/main/java/com/java/lld/design/patterns/singleton/ConnectionPool.java
@@ -2,12 +2,19 @@ package com.java.lld.design.patterns.singleton;
 
 public class ConnectionPool {
     
-    private static ConnectionPool INSTANCE = new ConnectionPool();
+    private static ConnectionPool INSTANCE;
     private ConnectionPool() {
         
     }
     
     public static ConnectionPool getInstance() {
+        if(INSTANCE == null) {
+            synchronized (ConnectionPool.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new ConnectionPool();
+                }
+            }
+        }
         return INSTANCE;
     }
 }
